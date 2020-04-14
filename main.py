1816e567353facc6cce9c598cf701f44266d29a9
An Do <ando.devel@gmail.com>
2020-04-15 00:49:36 +0700

    Apply superpixel after crfasrnn

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def iterate(file_path):
     probs = model.predict(img_data, verbose=False)[0]
     probs = probs.argmax(axis=2).astype('uint8')[:img_h, :img_w]
     probs = np.array(Image.fromarray(probs.astype('uint8')).resize((h, w)))
-    # Remove noise and expand neighbors.
+    # Remove noise and expand a bit to the 'background'
     probs = cv2.erode(probs, None, iterations=3)
     probs = cv2.dilate(probs, None, iterations=5)
     probs = np.expand_dims(probs, -1)
@@ -55,25 +55,33 @@ def iterate(file_path):
     from skimage.util import img_as_ubyte
 
     img = image
-    segments_fz = felzenszwalb(img, scale=100, sigma=0.5, min_size=30)
+    segments_fz = felzenszwalb(img, scale=100, sigma=0.5, min_size=25)
 
     # loop over the unique segment values
     for (i, segVal) in enumerate(np.unique(segments_fz)):
         # construct a mask for the segment
-        print
-        "[x] inspecting segment %d" % (i)
         mask = np.zeros(image.shape[:2], dtype="uint8")
-        mask[segments_fz == segVal] = 255
+        current_segment = segments_fz == segVal
+        mask[current_segment] = 1
+        sum_mask = mask.sum()
+        # mask[segments_fz == segVal] = 255
         # show the masked region
-        display_two_images(mask, cv2.bitwise_and(image, image, mask=mask))
+        # display_two_images(mask, cv2.bitwise_and(image, image, mask=mask))
+        mask = mask & np.squeeze(probs)
+        sum_probs = mask.sum()
+        # if sum_mask > sum_probs:
+        if sum_probs / sum_mask < 0.7:
+            # means current segment does not belong to foreground.
+            probs[current_segment] = 0
 
-    print(f"Felzenszwalb number of segments: {len(np.unique(segments_fz))}")
-    print(f"segments_fz's shape {segments_fz.shape}")
-    image_fz = img_as_ubyte(mark_boundaries(image_crf_splashed, segments_fz))
+    # print(f"Felzenszwalb number of segments: {len(np.unique(segments_fz))}")
+    # print(f"segments_fz's shape {segments_fz.shape}")
+    # image_fz = img_as_ubyte(mark_boundaries(image_crf_splashed, segments_fz))
     # ------ end section: superpixel ------
 
-    image_fz = apply_image_mask(probs, image_fz, [0, 0, 0])
-    display_single_image(image_fz)
+    # image_fz = apply_image_mask(probs, image_fz, [0, 0, 0])
+    image_final = apply_image_mask(probs, image, [0, 0, 0])
+    display_two_images(image, image_final)
 
 
 if __name__ == "__main__":
